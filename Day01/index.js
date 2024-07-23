07f2081490a88a680c374444dc9040380dbef0d8
Harshaa1818 <114155862+Harshaa1818@users.noreply.github.com>
2024-07-23 23:34:25 +0530

    2nd

--- Day01/index.js
+++ Day01/index.js
@@ -1,4 +1,4 @@
-//Flatten the object
+// -->> Flatten the object
 const obj={
     name: 'Harshal',
     age: 26,
@@ -33,10 +33,10 @@ function flatten(obj){
 
 }
 
-// console.log(flatten(obj));
+ console.log(flatten(obj));
 
 
-//Array Destructuring
+// -->> Array Destructuring
 
 const arr=[1,2,3,4,5]
 
@@ -44,10 +44,44 @@ const [a,b,c,d]=[10,...arr]
 
 console.log(d);
 
-//object destructuring
+// -->> object destructuring
 const {email}=req.body;
+const obj1={
+    name:"harshal"
+}
+const obj2={
+    city:"pune"
+}
 
-//dot operator
+console.log({...obj1,...obj2})
+
+// --> rest operator 
+
+function dummy(...rest){ //...rest is a rest operator here it gathers all the value and stores in rest variable
+    console.log(rest)
+
+}
+
+dummy(...arr) // ...arr is a spread operator here, which spreads all the array an pass it into a fn
+
+
+// --> Deep copy and shallow copy
+
+let obj3=obj1; // SHALLOW COPY, in this case obj3 will hold refrence of obj1 , whenever we mke change in obj1 it will reflect in obj3, 
+
+let obj4={...obj1} // DEEP COPY, it creates completely different object.
+
+// --> function currying
+
+function calculateVolume(length) {
+    return function (breadth) {
+        return function (height) {
+            return length * breadth * height;
+        }
+    }
+}
+console.log(calculateVolume(4)(5)(6));
+
 
 
 
