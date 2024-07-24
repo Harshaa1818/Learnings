49f537483faaa6da8acd83032e9948f4108c6397
Harshaa1818 <114155862+Harshaa1818@users.noreply.github.com>
2024-07-24 22:09:01 +0530

    day2

---
+++ Day02/dataTypes.js
@@ -0,0 +1,13 @@
+
+// Js has Primitive and not primitive data types
+// primitive data type are prdefined data types 
+// Number, string, boolean, null, undefined, bigInt, symbol
+// Non primitve data types are derived from primitive data type
+// e.g. array,Object, function 
+// -->everything in js in an object
+
+console.log({}=={})
+//it  will return false bcoz objects are compared by their refrence
+
+
+
