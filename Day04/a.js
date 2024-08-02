b1a48e2bb3eff271315e039538be3713e64c60e2
Harshaa1818 <114155862+Harshaa1818@users.noreply.github.com>
2024-08-03 00:08:04 +0530

    interview ques a.js

---
+++ Day04/a.js
@@ -0,0 +1,16 @@
+// let str=" a aa aabb abababab abb"
+
+// function find(str){
+
+//     let res=str.split(" ")
+
+//     let ans=res.sort((a,b)=>{
+//         if(a.length > b.length) return -1;
+//         else return 1;
+//     })
+//      return ans[0];
+
+// }
+// console.log(find(str))
+
+//
