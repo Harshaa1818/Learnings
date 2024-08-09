9147f9b1860633408a92f49cb2292a4bd193030c
Harshaa1818 <114155862+Harshaa1818@users.noreply.github.com>
2024-08-09 14:23:12 +0530

    allignement of a spcl div

--- Day07/todo-app-using-redux/src/App.jsx
+++ Day07/todo-app-using-redux/src/App.jsx
@@ -7,7 +7,7 @@ import Todo from './Todo.jsx';
 function App() {
   return (
     <>
-      <h1 style={{ textAlign: 'center', color: 'blue' }}>Todo App using Redux Toolkit</h1>
+      <h1 style={{ textAlign: 'center', color: 'black' }}>Todo App using Redux Toolkit</h1>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <Addtodo />
         <Todo />
--- Day07/todo-app-using-redux/src/Todo.jsx
+++ Day07/todo-app-using-redux/src/Todo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { removeTodo } from './redux/slices/TodoSlice'
+import './App.css'
 
 function Todos() {
   const dispatch=useDispatch();
@@ -8,11 +9,14 @@ function Todos() {
 
 return (
     <div>
-        <h1 style={{textAlign: 'center', color: 'blue'}}>Todos</h1>
-        <ul style={{listStyleType: 'none', padding: 0}}>
+        <h1 style={{textAlign: 'center', color: 'grey'}}>Todos</h1>
+        <ul style={{display: "flex", flexDirection:"column"}}>
             {todos && todos.map((todo) => (
-                <li key={todo.id} style={{marginBottom: '10px', backgroundColor: 'lightgray', padding: '5px'}}>
-                    {todo.text}
+                <li key={todo.id} style={{marginBottom: '10px', display:"flex", justifyContent:"space-between" , gap:"20px", width:"50vw",backgroundColor: 'lightgray', padding: '5px'}}>
+
+                    <div className='spcl-div'>
+                     {todo.text} 
+                     </div>
                     <button onClick={() => dispatch(removeTodo(todo.id))} style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', marginTop: '10px'}}>Remove Item</button>
                 </li>
             ))}
